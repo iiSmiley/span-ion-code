d4ffaacf08e333762113892a89b1a2d627996d48
Lydia Lee <lydiathelee@gmail.com>
2021-10-19 13:00:20 -0700

    [ll] updating gpib control

--- testing/gpib.py
+++ testing/gpib.py
@@ -1,41 +1,52 @@
-import visa
+import pyvisa
 import serial
 
 Z_INF = 'INF'
 Z_FIFTY = '50OHM'
 
-def arb_open(arb_name):
+def rsrc_open(rm, rsrc_name):
 	'''
 	Inputs:
-		arb_name: String. Name to use in the connection for the waveform 
-			generator, e.g. "USB0::0x0957::0x2C07::MY57801384::0::INSTR"
-		output_num: Integer; likely 1 or 2. The output source used for testing
-			for waveform generators with multiple output sources.
+		rm: PyVISA Resource manager.
+		rsrc_name: String. Name to use in the connection for the hardware, 
+			e.g. "USB0::0x0957::0x2C07::MY57801384::0::INSTR" or 
+			"GPIB0::5::INSTR"
 	Returns:
-		arb: Visa Resource object. The device to be opened.
-		rm: Visa ResourceManager object. The resource manager with which to 
-			open interfaces with various GPIB elements.
+		rsrc: Visa Resource object. The device to be opened.
 	'''
-	# Interface with voltage supply
-	rm = visa.ResourceManager()
-	arb = rm.open_resource(meas_name)
+	is_correct = False
+	while not is_correct:
+		# List resources for user selection by index
+		print('Index: Resource ID')
+		for i, x in enumerate(rm.list_resources()):
+			print(f'{i}: {x}')
+		idx_str = input('Choose Device Index: ')
+		rsrc = rm.open_resource(rm.list_resources[int(idx)])
 
-	# Sanity checking that it's the correct device
-	arb.query("*IDN?")
+		# Sanity checking that it's the correct device
+		rsrc.query("*IDN?")
+		is_correct_raw = input('Is this the correct device? (y/n): ').lower()
+		is_correct = (is_correct_raw == 'y')
 
-	return arb, rm
+		# If not correct, close the connection and prompt again
+		if not is_correct:
+			rsrc.close()
+
+	return rsrc
 
 def arb_config(arb, output_num, zin):
 	'''
 	Inputs:
-		arb:
-		output_num:
-		zin: 
+		arb: Visa Resource object. The opened device.
+		output_num: Integer. Index of the output source, i.e. 1 or 2 in our case.
+		zin: String gpib.Z_INF or gpib.Z_FIFTY (see above). Not sure what
+			this will do if it attempts to set to something invalid, but
+			it probably won't be pretty.
 	Returns:
-		
+		None.
 	'''
 	arb.write(f"OUTPUT{output_num}:LOAD {zin}")
-
+	return
 
 def arb_close(arb):
 	'''
@@ -43,8 +54,11 @@ def arb_close(arb):
 	Inputs:
 		arb: Visa Resource object. The device with an opened connection
 			to be closed.
+	Returns:
+		None.
 	'''
 	arb.write("OUTPUT1 OFF")
 	arb.write("OUTPUT2 OFF")
 
 	arb.close()
+	return
