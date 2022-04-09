bc4d3343600214050bfbda248b2823245a1ef2e6
Lydia Lee <lydiathelee@gmail.com>
2022-04-08 23:59:44 -0700

    [ll] ALL THE DATA

--- testing/run_me.py
+++ testing/run_me.py
@@ -184,9 +184,11 @@ def run_main():
 			yaml.dump(overflow_dict, outfile, default_flow_style=False)
 
 	####################################
-	### Small Chain ZCD Fast Testing ###
+	### Small Chain Fast Testing ###
 	####################################
 	if True:
+		f_atten = -10 # dB; CHANGE THIS WITH HARDWARE
+
 		asc_params = dict(
 			# MSB -> LSB TODO check that autozero gain...
 			preamp_res 		= [0, 0],
@@ -194,35 +196,36 @@ def run_main():
 			watchdog_res 	= [0, 0, 0, 0],
 			attenuator_sel	= [0, 0, 0],
 			dac_sel 		= [1, 1, 1, 1, 1, 1, 1, 1],
-			az_main_gain 	= [0]*3,
-			az_aux_gain 	= [0]*3,
+			az_main_gain 	= [1]*3,
+			az_aux_gain 	= [1]*3,
 			oneshot_res 	= [0, 0],
 			vref_preamp 	= [0, 0, 0, 0, 0, 0, 0, 0],
 			vdd_aon			= [0, 0, 0, 0, 0],
 			vdd_signal		= [0, 0, 0, 0, 0],
 			en_main			= [0],
-			en_small		= [1])
+			en_small		= [0])
 
 		vin_amp_vec = np.arange(0.7, 1.3, 50e-3)
+		# vin_amp_vec = [0.7, 1.3]
 		test_tdiff_small_params = dict(
 			teensy_port='COM5',
 			num_iterations=500,
 			asc_params=asc_params,
 			ip_addr='192.168.1.4',
 			gpib_addr=15,
-			vin_bias=0.0,
+			vin_bias=0,
 			tref_clk=1/3.75e6)
 
 		for vin_amp in vin_amp_vec:
 			timestamp = datetime.now()
 			timestamp_str = timestamp.strftime("%Y%m%d_%H%M%S")
-			file_out = f'../../data/testing/{timestamp_str}_vin{round(vin_amp, 2)}V_{test_tdiff_small_params["num_iterations"]}x_zcdSmall.yaml' 
-			
+			file_out = f'../../data/testing/{timestamp_str}_vin{round(vin_amp, 2)}V_{test_tdiff_small_params["num_iterations"]}x_vb{test_tdiff_small_params["vin_bias"]}V_zcdOnly.yaml'
 			test_tdiff_small_params['vin_amp'] = float(vin_amp)
 			tdiff_vec = testing.test_tdiff_small(**test_tdiff_small_params)
 			tdiff_vec = [float(tdiff) for tdiff in tdiff_vec]
 
 			dump_data = dict(config=test_tdiff_small_params,
+				f_atten=f_atten,
 				data=tdiff_vec)
 			with open(file_out, 'w') as outfile:
 				yaml.dump(dump_data, outfile, default_flow_style=False)
